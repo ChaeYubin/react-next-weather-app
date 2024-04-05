6856480e20cf8facd5ff1da88e6eb0df38e970ca
yubin <cyoobin@naver.com>
2024-04-05 21:48:13 +0900

    feat: 도시 데이터 요청 API 추가

--- src/components/CitySearchBar/CitySearchBar.tsx
+++ src/components/CitySearchBar/CitySearchBar.tsx
@@ -1,3 +1,93 @@
-export default function CitySearchBar() {
-  return <div>CitySearchBar</div>;
-}
+import { useQuery } from "@tanstack/react-query";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { WeatherDbData } from "@/models/weatherDbData";
+
+const CitySearchBar = () => {
+  const [search, setSearch] = useState<string>("");
+  const [isShowingSearchResults, setIsShowingSearchResults] =
+    useState<boolean>(false);
+
+  const cities = useQuery({
+    queryFn: async () => {
+      if (!search) {
+        setIsShowingSearchResults(false);
+        return [];
+      }
+      try {
+        const fetchedCities = await axios.get<WeatherDbData[]>(
+          `/api/cities/${search}`
+        );
+
+        if (fetchedCities.status !== 200) {
+          console.error(fetchedCities);
+          return [];
+        }
+
+        setIsShowingSearchResults(true);
+        return fetchedCities.data;
+      } catch (e: unknown) {
+        console.error(e);
+      }
+    },
+    queryKey: [search.toLowerCase()],
+    refetchOnMount: true,
+    refetchOnWindowFocus: false,
+  });
+
+  useEffect(() => {
+    cities.refetch();
+  }, [search]);
+
+  return (
+    <div className="searchBar">
+      <div
+        className={`searchBar__bar flex justify-between w-full max-w-[300px] border-2 relative`}
+      >
+        <input
+          type="text"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          placeholder={cities.isLoading ? "Loading..." : "Search city..."}
+          className="searchBar__input w-full h-[48px] text-[1rem] px-3 py-2"
+        />
+        <div className="searchBar__loadingSpinner absolute right-0 top-0  w-[48px] h-[48px] grid content-center">
+          {cities.isLoading || cities.isRefetching ? (
+            <div>로딩 스피너</div>
+          ) : // <LoadingSpinner width="w-[38px]" height="h-[38px]" />
+          null}
+        </div>
+      </div>
+
+      {isShowingSearchResults ? (
+        <div
+          className={`searchBar__results absolute z-[2] flex flex-col gap-2 border-2 
+            border-gray-500 my-1 rounded-[10px] p-2 w-[300px] bg-white`}
+        >
+          {/* {cities.data && cities.data.length !== 0 ? (
+            cities.data.slice(0, 6).map((city) => (
+              <CitySearchBarItem
+                city={city}
+                key={city.id}
+                onClick={() => {
+                  setIsShowingSearchResults(false);
+                  setSearch("");
+                }}
+              />
+            ))
+          ) : (
+            <CitySearchBarItem
+              noResultText="No results"
+              onClick={() => {
+                setIsShowingSearchResults(false);
+                setSearch("");
+              }}
+            />
+          )} */}
+        </div>
+      ) : null}
+    </div>
+  );
+};
+
+export default CitySearchBar;
