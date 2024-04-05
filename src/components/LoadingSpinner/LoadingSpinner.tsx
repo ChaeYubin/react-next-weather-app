385217a33791fb060e1391aa270af3080af96be1
yubin <cyoobin@naver.com>
2024-04-05 22:01:43 +0900

    feat: 로딩 스피너 추가

---
+++ src/components/LoadingSpinner/LoadingSpinner.tsx
@@ -0,0 +1,18 @@
+import styles from "./LoadingSpinner.module.css";
+
+interface LoadingSpinnerProps {
+  width?: string;
+  height?: string;
+}
+
+const LoadingSpinner = ({ width, height }: LoadingSpinnerProps) => {
+  return (
+    <div
+      className={`${width ?? "w-[48px]"} ${height ?? "h-[48px]"} ${
+        styles.loader
+      }`}
+    ></div>
+  );
+};
+
+export default LoadingSpinner;
